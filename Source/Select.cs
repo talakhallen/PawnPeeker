a5ccceeefebc6f64f993d43e1d6cd24b0c482308
talakhallen <79050176+talakhallen@users.noreply.github.com>
2021-04-17 20:51:37 -0400

    Add world pawn support to Select.TrySelect

--- Source/Select.cs
+++ Source/Select.cs
@@ -13,17 +13,28 @@ namespace PawnPeeker
     {
         public static bool TrySelect(Pawn pawn)
         {
-            if (!Find.Selector.IsSelected(pawn))
+            if (!pawn.IsWorldPawn())
             {
-                // TODO: Restore the previous selection after peeking.
-                Find.Selector.ClearSelection();
-
-                // World pawns cannot be selected.
-                if (!pawn.IsWorldPawn())
+                if (!Find.Selector.IsSelected(pawn))
                 {
                     Debug.Log(string.Format("Select {0}!", pawn.Name));
 
+                    // TODO: Restore the previous selection after peeking.
+                    Find.Selector.ClearSelection();
+
                     Find.Selector.Select(pawn);
+                }
+            }
+            else
+            {
+                if (!Find.WorldSelector.IsSelected(pawn.GetCaravan()))
+                {
+                    Debug.Log(string.Format("Select {0}!", pawn.Name));
+
+                    // TODO: Restore the previous selection after peeking.
+                    Find.WorldSelector.ClearSelection();
+
+                    Find.WorldSelector.Select(pawn.GetCaravan());
                 }
             }
 
